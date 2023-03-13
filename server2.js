10d6f227294a0a6a8bf9cf5c8ffc7fce04b92790
장루빈집 <wkdfnqls2465@gmail.com>
2023-03-13 22:54:02 +0900

    괄호 잘못 위치함

--- server2.js
+++ server2.js
@@ -1,6 +1,6 @@
-// 내장 모듈 http
-const http = require('http');
-// request, response 값을 담아서 함수 실행
-const server = http.createServer((request, response)) => {
+// 내장  모듈 http
+const http = require("http");
+//모든 node 웹 서버 어플은 웹 서버 객체가 필요, createServer 메서드를 이용해 만들어 줌
+const server = http.createServer((request, response) => {
   //  여기서 작업이 진행 된다
-}
+});
